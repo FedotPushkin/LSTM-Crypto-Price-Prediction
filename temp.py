b5ba1e0490c7d34d13ae703f656e4ec4efe7a5ba
FedotPushkin <d.sidorov54@gmail.com>
2024-05-14 15:59:17 +0300

    deleted useless temp files

---
+++ temp.py
@@ -0,0 +1,9 @@
+#import tensorflow as tf
+#from tensorflow.python.platform import build_info as tf_build_info
+
+#print(tf_build_info.cudnn_version_number)
+#print("cudnn_version",tf_build_info.build_info['cudnn_version'])
+#a = tf_build_info
+#print('')
+
+#print(tf.__version__)
