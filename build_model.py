629bebd17cc266676d6957419e142a43113c13f3
FedotPushkin <d.sidorov54@gmail.com>
2024-06-03 12:42:35 +0300

    experimented with model architecture

--- build_model.py
+++ build_model.py
@@ -5,41 +5,44 @@ from keras.models import Sequential
 from keras.layers import LSTM, Dense, Dropout, BatchNormalization, Attention  # , AlphaDropout
 
 
-def build_model(x_adj, learning_rate):
+def build_model(x_adj, regression, learning_rate):
 
-    regression = True
+    reg = 'l2'
     # first layer
     model = Sequential()
     model.add(LSTM(100,
                    input_shape=(x_adj.shape[1], x_adj.shape[2]),
                    return_sequences=True,
-                   recurrent_regularizer=reg.L1(0.005),
-                   kernel_regularizer=reg.L1(0.005)))   # recurrent_dropout=0.05))
+
+                   recurrent_regularizer=reg,
+                   kernel_regularizer=reg))   # recurrent_dropout=0.05))
     model.add(BatchNormalization())
-    model.add(Dropout(0.05))
+    #model.add(Dropout(0.05))
 
     # second layer
-    # model.add(LSTM(40, return_sequences=True,))# kernel_regularizer=reg.L1(0.001)))
-    # model.add(BatchNormalization())
-    # model.add(Dropout(0.05))
-    model.add(LSTM(100, return_sequences=False, recurrent_regularizer=reg.L1(0.005), kernel_regularizer=reg.L1(0.005)))
+    model.add(LSTM(100, return_sequences=True, recurrent_regularizer=reg, kernel_regularizer=reg))# kernel_regularizer=reg.L1(0.001)))
     model.add(BatchNormalization())
-    model.add(Dropout(0.05))
+    #model.add(Dropout(0.05))
+    model.add(LSTM(100, return_sequences=False, recurrent_regularizer=reg, kernel_regularizer=reg))  # , recurrent_regularizer='l2'))# , kernel_regularizer=reg.L1(0.005)))
+    model.add(BatchNormalization())
+    #model.add(Dropout(0.5))
 
-    model.add(Dense(20, activation='relu', kernel_regularizer=reg.L1(0.005)))
+    model.add(Dense(50, activation='relu', kernel_regularizer=reg))
     # model.add(AlphaDropout(0.5))
-    model.add(Dropout(0.05))
+    model.add(Dropout(0.1))
     # fourth layer and output
     # model.add(Dense(1, activation='linear'))    # kernel_regularizer=regularizers.L1(0.01)
 
-    # optimizer = keras.optimizers.Nadam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, decay=0.004)
+    #optimizer = keras.optimizers.Nadam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, decay=0.004)
 
-    model.add(Dense(1, activation='linear'))
+
     if regression:
+        model.add(Dense(1, activation='linear'))
         model.compile(loss='mse',
                       metrics=[keras.metrics.RootMeanSquaredError()],
                       optimizer='nadam')
     else:
+        model.add(Dense(2, activation='softmax'))
         model.compile(loss='binary_crossentropy',
                       metrics=['accuracy'],
                       optimizer='rmsprop')
@@ -49,36 +52,40 @@ def build_model(x_adj, learning_rate):
 
 def build_model_bayesian(hp):
     model = Sequential()
-    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=800, step=20),
-                                dropout=hp.Float('droput', min_value=0.15, max_value=0.99, step=0.05),
+    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=200, step=10),
+                                dropout=hp.Float('droput', min_value=0.05, max_value=0.99, step=0.01),
                                 recurrent_dropout=hp.Float('redroput', min_value=0.05, max_value=0.99, step=0.05),
                                 activation='relu',
                                 return_sequences=True,
                                 input_shape=(30, 1)))
     Attention()
-    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=800, step=20),
-                                dropout=hp.Float('droput', min_value=0.15, max_value=0.99, step=0.05),
+    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=200, step=10),
+                                dropout=hp.Float('droput', min_value=0.05, max_value=0.99, step=0.01),
                                 activation='relu', return_sequences=True))
     Attention()
-    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=800, step=20), activation='relu'))
-    model.add(keras.layers.Dense(1))
+    model.add(keras.layers.LSTM(units=hp.Int('units', min_value=40, max_value=200, step=10), activation='relu'))
+    model.add(Dense(2, activation='softmax'))
+    model.compile(loss='binary_crossentropy',
+                  metrics=['accuracy'],
+                  optimizer=keras.optimizers.Adam(
+                      hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4, 1e-7, 1e-10])))
 
-    model.compile(loss='mean_squared_error', optimizer=keras.optimizers.Adam(
-        hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4, 1e-7, 1e-10])))
     return model
 
 
-bayesian_opt_tuner = keras_tuner.BayesianOptimization(
-    build_model_bayesian,
-    objective='val_loss',
-    max_trials=200,
-    executions_per_trial=1,
-    project_name='timeseries_bayes_opt_POC',
-    overwrite=True, )
+
 
 
 def opt_search(x_adj, y_adj):
 
+
+    bayesian_opt_tuner = keras_tuner.BayesianOptimization(
+        build_model_bayesian,
+        objective='val_loss',
+        max_trials=200,
+        executions_per_trial=1,
+        project_name='timeseries_bayes_opt_POC',
+        overwrite=True, )
     bayesian_opt_tuner.search(x=x_adj, y=y_adj,
                               epochs=300,
                               # validation_data=(xval ,xval),
