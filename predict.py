c7dafdb409f1197ff53b68d1f8babe2309d3f406
FedotPushkin <d.sidorov54@gmail.com>
2024-06-03 12:43:56 +0300

    split into building xval and predicting whole, added timing

--- predict.py
+++ predict.py
@@ -1,56 +1,71 @@
 import numpy as np
 import pandas as pd
+import time
 from keras.models import load_model
 from build_data import build_val_data, shape_data
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 
-def predict(tag, params):
-    validation_length, validation_lag, timesteps = params[:-1]
-    calc_xval = False
+def predict_val(tag, params):
+    validation_length, validation_lag, timesteps, regression, calc_xval = params
     candles = pd.DataFrame(np.load('candles.npy', allow_pickle=True))
+    lstm = load_model(f'my_model.keras')
+    y_strat_p = list()
+    y_strat_n = list()
+    runs = 0
+    select, build, shape = 0, 0, 0
     if calc_xval:
-        y_strat_p = list()
-        # y_strat_p.append([])
-        y_strat_n = list()
+
         # y_strat_n.append([])
         # for models in range(1):
         # up, down = False, False
         # lstm = load_model(f'models/lstm_model_{tag}_{models}.h5')
 
-        x_val = list()
-        lstm = load_model(f'my_model.keras')
+        x_val = np.empty(shape=(1, timesteps, 13))
+        val_sample = 10000
         for v in range(validation_length - validation_lag - 2 * timesteps):
+            if v > val_sample:
+                continue
+            t0 = time.time()
             val_input = candles.iloc[-validation_length + v:-validation_length + v + validation_lag + timesteps]
-            x_val_single = build_val_data(data=val_input)
+            x_val_single = build_val_data(data=val_input, params=params)
             # graph(X, start_g=31, hold_g=y, len_g=50, col_type=0)
-            x_val_single, _ = shape_data(x_val_single, [0], training=False)
-            x_val.append(x_val_single)
+            x_val_single, _ = shape_data(x_val_single, [0], training=False, params=params)
+            x_val = np.append(x_val, x_val_single, axis=0)
+            t1 = time.time()
+            runs += 1
+            build += t1-t0
+            print(f'adding val data for  candle {v} of {val_sample}')
 
-            lstm_pred = lstm.predict(x_val_single, batch_size=1)
-            d = lstm_pred[0]
-            y_strat_p.append(d)
-            print(f'adding val data for  candle {v} of {validation_length}')
-            # close = candles.iloc[-validation_length + v + timesteps][1]
-            # open = candles.iloc[-validation_length + v + timesteps][0]
-            # re = (close - open) / open
-            # if d > 0:
-
-            # u = X_val[0][14][1]
-            # y_strat[models].append(u if (np.isnan(d)) else d)
-            # lstm_pred[0]
-        # np.save(f'X_val', X_val, allow_pickle=True)
-        y_strat_p = pd.DataFrame(y_strat_p)
-        y_strat_n = pd.DataFrame(y_strat_n)
-        np.save(f'y_strat_{tag}_p', y_strat_p, allow_pickle=True)
-        np.save(f'y_strat_{tag}_n', y_strat_n, allow_pickle=True)
+        x_val = np.delete(x_val, 0, axis=0)
+        np.save('x_val', x_val, allow_pickle=True)
     else:
-        # X_val = np.load(f'X_val.npy', allow_pickle=True)
-        print(f'xval loaded')
-        # newxval=list()
-    # for i in range(X_val.shape[0]):
-    # newxval.append(X_val[i][0])
-    # newxval = pd.DataFrame( np.array(newxval))
+        x_val = np.load("x_val.npy", allow_pickle=True)
+    t3 = time.time()
+    lstm_pred = lstm.predict(x_val, batch_size=4096)
+    t4 = time.time()
+    pred_time = t4 - t3
+    if regression:
+        y_strat_p = lstm_pred[0]
+    else:
+        y_strat_p = lstm_pred.T[1]
+
+
+    # close = candles.iloc[-validation_length + v + timesteps][1]
+    # open = candles.iloc[-validation_length + v + timesteps][0]
+    # re = (close - open) / open
+    # if d > 0:
+
+    # u = X_val[0][14][1]
+    # y_strat[models].append(u if (np.isnan(d)) else d)
+    # lstm_pred[0]
+    # np.save(f'X_val', X_val, allow_pickle=True)
+    print(f' build {build:2f} predict {pred_time:2f} ')
+    y_strat_p = pd.DataFrame(y_strat_p)
+    y_strat_n = pd.DataFrame(y_strat_n)
+    np.save(f'y_strat_{tag}_p', y_strat_p, allow_pickle=True)
+    np.save(f'y_strat_{tag}_n', y_strat_n, allow_pickle=True)
+
 
 
 def getpreds(tag):
