159541a96291af3505ae1be1337ca82c75095d42
FedotPushkin <d.sidorov54@gmail.com>
2024-06-12 11:49:00 +0300

    cut garbage

--- graph.py
+++ graph.py
@@ -32,13 +32,10 @@ def graph(lines, start_g, col_type, lag,  params):
     # for i in range(timesteps):
     #  pr.append(None)
     cols = [[0, 1, 2, 3, 4, 5, 6, 7], ['open', 'close', 'high', 'low', 'mean', 'vol', 'Date']]
-
+    zero = [0 in range(200)]
     # te_g = x_g.T[0]
     # open_g = x_g.T[4]
     # close_g = x_g.T[5]
-
-    zero = [0 for a in range(200)]
-
     # line_color = '#008000'
     trace0 = go.Candlestick(x=cand_g[cols[col_type][6]],
                             open=cand_g[cols[col_type][0]],
@@ -49,7 +46,7 @@ def graph(lines, start_g, col_type, lag,  params):
 
     trace1 = go.Scatter(x=cand_g[cols[col_type][6]], y=lines[3][lag:], name='holding', mode='lines', yaxis='y3',
                         line_color='red',)
-    #trace3 = go.Scatter(x=cand_g[cols[col_type][6]], y=lines[0][lag:], yaxis='y2',
+    # trace3 = go.Scatter(x=cand_g[cols[col_type][6]], y=lines[0][lag:], yaxis='y2',
     #                    name='not_used', mode='lines+markers', )
     # trace4 = go.Scatter(x=cand_g[cols[col_type][6]], y=holding_m[lag:], yaxis='y3',
     #                     name='holding_n', mode='lines+markers', line_color='#f44336' )
@@ -60,15 +57,8 @@ def graph(lines, start_g, col_type, lag,  params):
     trace9 = go.Scatter(x=cand_g[cols[col_type][6]], y=lines[0][preds_lag+lag:],
                         name='savgol', mode='lines+markers', line_color='cyan')
     trace10 = go.Scatter(x=cand_g[cols[col_type][6]], y=zero,
-                        name='zero', mode='lines', line_color='black', yaxis='y4',)
+                         name='zero', mode='lines', line_color='black', yaxis='y4',)
 
-    #trace6 = go.Candlestick(x=cand_g[cols[col_type][6]],
-                            # x=x_g.T[13][bias_x:len_g],
-                            #open=x_g.T[4][bias_x:len_g],
-                            #high=max_g(x_g.T[4][bias_x:len_g], x_g.T[5][bias_x:len_g]),
-                            #low=min_g(x_g.T[4][bias_x:len_g], x_g.T[5][bias_x:len_g]),
-                            #close=x_g.T[5][bias_x:len_g],
-                            #name='xtrain')
     # trace1 = go.Scatter(x=cand_g[cols[col_type][6]], y=x_g.T[0], name='macd', mode='lines',)
 
     # trace2 = go.Scatter(x=candles['Date'], y=self.savgol, name='Filter')
@@ -77,7 +67,7 @@ def graph(lines, start_g, col_type, lag,  params):
 
     # trace4 = go.Scatter(x=cand_g[cols[col_type][6]], y=x_g.T[3], name='psar_h', mode='lines', )
     # trace5 = go.Scatter(x=cand_g[cols[col_type][6]], y=x_g.T[4], name='psar_l', mode='lines', )
-    data = [trace0, trace1, trace8, trace10]
+    # data = [trace0, trace1, trace8, trace10]
     y2 = go.YAxis(overlaying='y', side='right')
     y3 = go.YAxis(overlaying='y', side='right')
     y4 = go.YAxis(overlaying='y', side='right')
@@ -89,7 +79,7 @@ def graph(lines, start_g, col_type, lag,  params):
         yaxis4=y4,
         yaxis5=y5
     )
-    graphs = [trace0, trace1, trace7, trace8, trace9, trace10]  # , trace8,,trace3]
+    graphs = [trace0, trace1,  trace8,  trace10]  # , trace8,,trace3]
     fig2 = go.Figure(data=graphs, layout=layout)
     # fig2 = make_subplots(rows=3, cols=1)
     # fig2.add_trace(trace0, row=1, col=1)
